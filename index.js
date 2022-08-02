3fb8bbaa6f28639ba4f8e8f5317ceb6e1286f427
Reese Pollard <rpollard@gmail.com>
2022-08-01 20:30:55 -0700

    Added MongoDB

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
 const { response } = require('express')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
+
 
 app.use(cors())
 app.use(express.json())
 app.use (express.static('build'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    imporant: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hellod World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
