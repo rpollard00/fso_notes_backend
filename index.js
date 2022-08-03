8ef5322fd2829ff8a13fcd276e65b271465f0726
Reese Pollard <rpollard@gmail.com>
2022-08-03 10:13:27 -0700

    Refactor middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const config = require("./utils/config")
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const notesRouter = require("./controllers/notes")
+const middleware = require("./utils/middleware")
 const Note = require("./models/note")
 const logger = require("./utils/logger")
 
@@ -9,93 +11,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static("build"))
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hellod World!</h1>")
-})
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
-})
-
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
-})
-
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.put("/api/notes/:id", (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedNote) => {
-      response.json(updatedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
-})
+app.use("/api/notes", notesRouter)
 
 // last middleware
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" })
-}
 
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === "CastError") {
-    response.status(400).send({ error: "malformed id" })
-  } else if (error.name === "ValidationError") {
-    return response.status(400).send({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT)
 logger.info(`Server running on port ${config.PORT}`)
