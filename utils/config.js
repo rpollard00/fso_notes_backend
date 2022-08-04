70dcbbf8bd386bde5aab5b0f490863a769ab5fa7
Reese Pollard <rpollard@gmail.com>
2022-08-04 13:40:53 -0700

    Implement tests

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require("dotenv").config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI_NOTES
+
+
+const MONGODB_URI = process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI_NOTES
+    : process.env.MONGODB_URI_NOTES
 
 module.exports = {
   MONGODB_URI,
