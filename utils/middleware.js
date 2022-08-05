628bf845bfa6e5aa8e28c14acefbcf239494493d
Reese Pollard <rpollard@gmail.com>
2022-08-05 15:40:07 -0700

    Implement token auth

--- utils/middleware.js
+++ utils/middleware.js
@@ -23,7 +23,15 @@ const errorHandler = (error, request, response, next) => {
     response.status(400).send({ error: "malformed id" })
   } else if (error.name === "ValidationError") {
     return response.status(400).send({ error: error.message })
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: 'invalid token ' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
+
+  logger.error(error.message)
 
   next(error)
 }
